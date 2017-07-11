c3f9904a3b0d8285da82bff1d15692392c05418f
akakou <s16003@tokyo.kosen-ac.jp>
2017-07-11 15:41:00 +0900

    [add] 色の変更

--- web/js/script.js
+++ web/js/script.js
@@ -5,11 +5,19 @@ function chat(){
   /* --- Get Comment ---- */
   socket.on('chat', function (msg) {
     var randnum = 20 + Math.floor( Math.random() * 61 );
-    var comment = document.createElement('Marquee');
-    comment.textContent = msg;
-    comment.loop = 1;
-    comment.style.position = "absolute"
-    comment.style.bottom = randnum.toString() + "%"
+    var comment = document.createElement('maquee');
+    var command = msg.substr(0,2);
+    if(command=="@r"){
+        comment.textContent = msg.substring(2,msg.length);
+        comment.style.color ="#993333";
+    }else{
+        comment.textContent = msg;
+        comment.style.color ="#000000";
+    }
+    comment.style.position = "absolute";
+    comment.style.left = "0%";
+    comment.style.top = randnum.toString() + "%";
+    comment.style.loop = 1;
     messages.appendChild(comment);
   });
 }
