27f4a52511df3c3c279690a15143bc9342b9c90c
akakou <s16003@tokyo.kosen-ac.jp>
2017-07-11 16:39:09 +0900

    [fix] スクロールするように修正

--- web/js/script.js
+++ web/js/script.js
@@ -1,23 +1,25 @@
 function chat(){
+//  var msg = "‚ ‚ ‚ ";
   var socket = io();
   var messages = document.getElementById('messages');
 
   /* --- Get Comment ---- */
   socket.on('chat', function (msg) {
-    var randnum = 20 + Math.floor( Math.random() * 61 );
-    var comment = document.createElement('maquee');
-    var command = msg.substr(0,2);
-    if(command=="@r"){
-        comment.textContent = msg.substring(2,msg.length);
-        comment.style.color ="#993333";
+    var randnum = Math.floor( Math.random() * 80 );
+    var comment = document.createElement('marquee');
+    var size = (msg.length);
+    if(msg.charAt(0)==">"){
+      comment.textContent = msg.substring(1,msg.length);
+      comment.style.color ="#cc3333";
     }else{
-        comment.textContent = msg;
-        comment.style.color ="#000000";
+      comment.textContent = msg;
+      comment.style.color ="#000000";
     }
+    comment.scrollamount = "10px";
+    comment.loop = 1;
     comment.style.position = "absolute";
-    comment.style.left = "0%";
     comment.style.top = randnum.toString() + "%";
-    comment.style.loop = 1;
+    comment.style.fontsize = "10%";
     messages.appendChild(comment);
   });
 }
