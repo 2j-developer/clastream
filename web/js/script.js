74d8f9f3b5bfcf3508ce713614689a0bdad29bbc
iwat0874 <1961twshelp1117@gmail.com>
2017-07-11 18:46:39 +0900

    [update] view用テスト。文中のスペースを減らす、実行時に表示する文字を求める、サイズ、スピードの調整

--- web/js/script.js
+++ web/js/script.js
@@ -1,25 +1,34 @@
 ﻿function chat(){
-  var msg = ".あああ";
+  var msg = prompt( "メッセージ" , "入力欄のデフォルト値" );
 //  var socket = io();
   var messages = document.getElementById('messages');
+
+
+  //スペース減らし byたかねこ
+  while(msg.indexOf("  ") != -1){
+    msg = msg.replace("  "," ");
+  }
 
   /* --- Get Comment ---- */
 //  socket.on('chat', function (msg) {
     var randnum = Math.floor( Math.random() * 80 );
     var comment = document.createElement('marquee');
-    var size = 500;
+    var max = 60
+    var param = msg.length < max ? msg.length : max ;
+    var size = 400-param*2;
+    var speed = 12+param;
+
+    //alert(""+size);
 
     if(msg.charAt(0)=="."){
       comment.textContent = msg.substring(1,msg.length);
-      comment.style.color ="#cc3333";
+      comment.style.color ="#cccc33";
     }else{
       comment.textContent = msg;
-      comment.style.color ="#000000";
     }
 
-    comment.scrollAmount = 12 + msg.length;
+    comment.scrollAmount = speed;
 //    comment.loop = 1;
-    comment.style.position = "absolute";
     comment.style.top = randnum.toString() + "%";
     comment.style.fontSize = size.toString() + "%";
     messages.appendChild(comment);
