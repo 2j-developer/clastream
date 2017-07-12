f06325f01704a2d7bd38902e26f56df7eae09d25
iwat0874 <1961twshelp1117@gmail.com>
2017-07-12 14:52:07 +0900

    [update] ファイルの分割

---
+++ web/js/pop.js
@@ -0,0 +1,20 @@
+﻿function createPop(num){
+  var pop = document.createElement('img');
+  var i = 0;
+  while(document.getElementById("pop" + i)){i++;}
+  pop.id = "pop" + i;
+  pop.src = "./icons/" + list[num] + ".png";
+  if(Math.random()<0.5){
+    pop.style.top = (Math.floor( Math.random() * 50 )).toString() + "%";
+    pop.style.left = (Math.floor( Math.random() * 50 )).toString() + "%";
+  }else{
+    pop.style.bottom = (Math.floor( Math.random() * 50 )).toString() + "%";
+    pop.style.right = (Math.floor( Math.random() * 50 )).toString() + "%";
+  }
+  messages.appendChild(pop);
+  setTimeout("killPop("+ i +")",5000);
+}
+function killPop(num){
+  var popid = document.getElementById("pop"+num);
+  popid.parentNode.removeChild(popid);
+}
--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,4 @@
-﻿var list = ["fire","+1","-1","a","b"];
-function chat(){
+﻿function chat(){
 //マージするときはこの下の行はコメントアウト
   var msg = prompt( "" , "流すメッセージを入力" );
 
@@ -71,23 +70,3 @@ function createMarquee(msg){
   }
 }
 
-function createPop(num){
-  var pop = document.createElement('img');
-  var i = 0;
-  while(document.getElementById("pop" + i)){i++;}
-  pop.id = "pop" + i;
-  pop.src = "./icons/" + list[num] + ".png";
-  if(Math.random()<0.5){
-    pop.style.top = (Math.floor( Math.random() * 50 )).toString() + "%";
-    pop.style.left = (Math.floor( Math.random() * 50 )).toString() + "%";
-  }else{
-    pop.style.bottom = (Math.floor( Math.random() * 50 )).toString() + "%";
-    pop.style.right = (Math.floor( Math.random() * 50 )).toString() + "%";
-  }
-  messages.appendChild(pop);
-  setTimeout("killPop("+ i +")",5000);
-}
-function killPop(num){
-  var popid = document.getElementById("pop"+num);
-  popid.parentNode.removeChild(popid);
-}
