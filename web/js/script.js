8c590c58ebbbeb80067e5615c6028ee0e5beaf06
iwat0874 <1961twshelp1117@gmail.com>
2017-07-12 12:33:45 +0900

    [fix] アイコンと色の修正

--- web/js/script.js
+++ web/js/script.js
@@ -28,8 +28,8 @@ function createMarquee(msg){
   //タグ作る
   var comment = document.createElement('marquee');
   var max = 60;
-  var ccode = ["r","g","b","c","m","y"];
-  var color = ["#ff0000","#00ff00","#0000ff","#00ffff","#ff00ff","#fffff00"];
+  var ccode = ["r","g","b","c","m","y","w"];
+  var color = ["#ff0000","#00ff00","#0000ff","#00ffff","#ff00ff","#ffff00","#ffffff"];
 
   //スペース減らし byたかねこ
   //全角全角→半角
